6f975a0dcf88655cf8bc7ee0961698177ab27e42
AlexOuttaspace <matanforeveryone@gmail.com>
2018-06-08 20:09:38 +0300

    App conteiner test initial setup

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Sound from 'react-sound';
+
+import Controls from './components/Controls/Controls';
+import Playlist from './components/Playlist/Playlist';
+import SearchBar from './components/SeachBar/SearchBar';
+import MaterialCard from './components/UI/MaterialCard/MaterialCard';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import Spinner from './components/UI/Spinner/Spinner';
+
+describe('App container', () => {});
